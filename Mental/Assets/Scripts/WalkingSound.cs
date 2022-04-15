dd305202786a9705a6236f5fa6164484d7a3ad2c
mads juhl <majuh18@student.sdu.dk>
2022-04-15 23:15:20 +0200

    ss

---
+++ Mental/Assets/Scripts/WalkingSound.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingSound : MonoBehaviour
+{
+    public List<AudioClip> walkingSounds = new List<AudioClip>();
+
+    public AudioSource audioSource;
+}
