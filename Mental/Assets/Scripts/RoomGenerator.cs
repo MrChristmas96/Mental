10fbc27408aaf63ffbe409b9f794971eda83b684
Macoh20 <macoh20@student.sdu.dk>
2022-05-03 14:36:02 +0200

    Fixed Rooms & Loop (Ikke længere tryk "space")

--- Mental/Assets/Scripts/RoomGenerator.cs
+++ Mental/Assets/Scripts/RoomGenerator.cs
@@ -10,15 +10,7 @@ public class RoomGenerator : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            for(int i = 0; i < roomABC.Length; i++)
-            {
-                int r = Random.Range(0, rooms.Length);
-                Instantiate(rooms[r], roomABC[i].transform.position, Quaternion.identity);
-            }
 
-        }
         if (Input.GetKeyDown("z"))
         {
             GameObject[] destroy = GameObject.FindGameObjectsWithTag("Room");
@@ -27,6 +19,21 @@ public class RoomGenerator : MonoBehaviour
                 Destroy(room);
             }
            
+        }
+    }
+
+    public void CreateRoom()
+    {
+        Debug.Log("Generated New Rooms");
+        GameObject[] destroy = GameObject.FindGameObjectsWithTag("Room");
+        foreach (GameObject room in destroy)
+        {
+            Destroy(room);
+        }
+        for (int i = 0; i < roomABC.Length; i++)
+        {
+            int r = Random.Range(0, rooms.Length);
+            Instantiate(rooms[r], roomABC[i].transform.position, Quaternion.identity);
         }
     }
 }
